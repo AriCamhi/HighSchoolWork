54ac068c3b15702b277e0e814b5685c3a991ae50
AriCamhi <arijcamhi@gmail.com>
2022-09-27 21:59:38 -0400

    Some changes

--- RandomStuff/src/main/java/SecondLab.java
+++ RandomStuff/src/main/java/SecondLab.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 /**
  * Second Lab with 4 methods for ATCS
  *
@@ -6,6 +8,15 @@
  */
 public class SecondLab {
 
+    /**
+     * Method 1:
+     *
+     * Method that should return all possible combinations
+     * of change given an amount of cents.
+     *
+     * @param cents in between 1 and 100
+     * @return all possible combinations
+     */
     public String coinCombinations(int cents) {
         int quarters = 0;
         int dimes = 0;
@@ -35,14 +46,89 @@ public class SecondLab {
         return j;
     }
 
-//    public String seventeen76(){
-//        int num = 1;
-//        int denom = 1;
-//        int desired = 17 / 76;
-//
-//    }
+    /**
+     * Method 2:
+     *
+     * Method to calculate the price of two textbooks given their special
+     * discounts
+     * @param bp Be Prepared
+     * @param nb Next Best
+     * @return total price
+     */
+    public double calculateBookTotal(int bp, int nb) {
+        double priceBp = 18.95 * bp;
+        double priceNb = 21.95 * nb;
+
+        double total = 0;
+        total = priceBp + priceNb;
+
+        if (bp == 1 && nb == 1) {
+            total = 37.95;
+        }
+
+        if (bp + nb >= 3) {
+            total = 15.95 * (bp + nb);
+        }
+
+        if (bp + nb >= 12) {
+            total = 14 * (bp + nb);
+        }
+        DecimalFormat form1 = new DecimalFormat("0.00");
+        System.out.println("Total Price: " + form1.format(total));
+        return Double.parseDouble(form1.format(total));
+    }
 
     /**
+     * Method 3:
+     *
+     * Method to return the two closet fractions to 17/76
+     * One should be below and one should be above
+     *
+     * @return both fractions from below and above
+     */
+    public String seventeen76() {
+        //Loops for finding the fraction below:
+        double reference = 17. / 76;
+        double target = 0.0;
+        int goodNum = 0;
+        int goodDenom = 0;
+        for (int num = 1; num <= 100; num++) {
+            for (int denom = 1; denom <= 100; denom++) {
+               double newValue = (double) num / denom;
+               if (newValue < reference) {
+                   if (newValue > target) {
+                       goodNum = num;
+                       goodDenom = denom;
+                       target = newValue;
+                   }
+               }
+            }
+        }
+        //Loops for finding the fraction above:
+        double target2 = 0.3;
+        int betterNum = 0;
+        int betterDenom = 0;
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                double newValueAgain = (double) i / j;
+                if (newValueAgain > reference) {
+                    if (newValueAgain < target2) {
+                        betterNum = i;
+                        betterDenom = j;
+                        target2 = newValueAgain;
+                    }
+                }
+            }
+        }
+        System.out.println("Fraction below: " + goodNum + "/" + goodDenom);
+        System.out.println("Fraction above: " + betterNum + "/" + betterDenom);
+        String output = "test";
+        return output;
+    }
+
+    /**
+     * Method 4:
+     *
      * Method to return the stars in the shape of an upside down
      * pyramid. Using a StringBuilder instead of just a string to make
      * it easier. Example: int n = 5
@@ -51,7 +137,7 @@ public class SecondLab {
      *   ***
      *  *****
      *
-     * @param n
+     * @param n of stars
      * @return pic of pyramid
      */
     public StringBuilder starPyramid ( int n){
--- RandomStuff/src/test/java/SecondLabTester.java
+++ RandomStuff/src/test/java/SecondLabTester.java
@@ -13,13 +13,27 @@ public class SecondLabTester {
     SecondLab s = new SecondLab();
 
     @Test
+    public void testingCoinCombos(){
+        assert(s.coinCombinations(74).equals("hello"));
+    }
+
+    @Test
+    public void testingCalculateBookTotal(){
+        assert(s.calculateBookTotal(6, 8)==196);
+        assert(s.calculateBookTotal(1,1)==37.95);
+        assert(s.calculateBookTotal(1,2)==47.85);
+        assert(s.calculateBookTotal(4,3)==111.65);
+    }
+
+    @Test
+    public void testingSeventeen76(){
+        assert(s.seventeen76().equals("test"));
+    }
+
+    @Test
     //This works, but it says that the test if failed
     public void testingStarPyramid() {
         assert(s.starPyramid(5).equals("It still works, don't know how to assert"));
     }
 
-    @Test
-    public void testingCoinCombos(){
-        assert(s.coinCombinations(74).equals("hello"));
-    }
 }

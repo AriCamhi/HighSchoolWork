953ded70d7be3421f0be850f3fa55103fde95fe2
AriCamhi <arijcamhi@gmail.com>
2022-09-30 19:48:23 -0400

    Finished Coin Lab

--- RandomStuff/src/main/java/Coins.java
+++ RandomStuff/src/main/java/Coins.java
@@ -12,6 +12,22 @@ public class Coins {
     private int pennies;
     private int cents;
 
+    public void setQuarters(int quarters) {
+        this.quarters = quarters;
+    }
+
+    public void setDimes(int dimes) {
+        this.dimes = dimes;
+    }
+
+    public void setNickels(int nickels) {
+        this.nickels = nickels;
+    }
+
+    public void setPennies(int pennies) {
+        this.pennies = pennies;
+    }
+
     public int getQuarters() {
         return quarters;
     }
@@ -35,6 +51,12 @@ public class Coins {
         this.cents = cents;
     }
 
+    /**
+     * Method that finds the optimal amount of change with the least
+     * amount of coins possible
+     * @param cents of the given coin
+     * @return total amount of coins needed to represent the number of cents
+     */
     public int optimalChange(int cents) {
         //Finds the optimal change
         //The least amount of coins possible
@@ -57,29 +79,61 @@ public class Coins {
                 cents = 0;
             }
         }
-        System.out.println("Quarters: " + quarters + " Dimes: " + dimes + " Nickels: " + nickels
-                + " Pennies: " + pennies);
+        setQuarters(quarters);
+        setDimes(dimes);
+        setNickels(nickels);
+        setPennies(pennies);
         int totalCoins = quarters + dimes + nickels + pennies;
         return totalCoins;
     }
 
+    /**
+     * A toString method to print out the optimal change for
+     * a given coin.
+     * @return string representation of the coin
+     */
     public String toString() {
-
-        System.out.println("Quarters: " + getQuarters() + " Dimes: " + getDimes() + " Nickels: " + getNickels()
-                + " Pennies: " + getPennies());
-        return "Quarters: " + getQuarters() + " Dimes: " + getDimes() + " Nickels: " + getNickels()
-                + " Pennies: " + getPennies();
+        Coins c1 = new Coins();
+        c1.optimalChange(cents);
+        return "Quarters: " + c1.getQuarters() + " Dimes: " + c1.getDimes() + " Nickels: " + c1.getNickels()
+                + " Pennies: " + c1.getPennies();
     }
 
 
+    /**
+     * Method to compare two coin objects, print out
+     * 0 if they are equal, 1 if the first object has more coins,
+     * and -1 if the first coin has less coins than the second
+     * @param c2, a second coin object to compare the first one to
+     */
     public void compareTo(Coins c2) {
         Coins c1 = new Coins(cents);
-        if (c1.optimalChange(cents) > c2.optimalChange(cents)) {
+        String firstCoinString = c1.toString();
+        String secondCoinString = c2.toString();
+        if (firstCoinString.compareTo(secondCoinString) > 0) {
             System.out.println(1);
-        } else if (c1.optimalChange(cents) == c2.optimalChange(cents)) {
+        } else if (firstCoinString.compareTo(secondCoinString) == 0) {
             System.out.println(0);
         } else {
             System.out.println(-1);
+        }
+    }
+
+    /**
+     * A method to compare two coin objects to see if they
+     * are directly equivalent
+     *
+     * @param c2, a second coin object to compare the first one to
+     */
+    public void equals(Coins c2){
+        Coins c1 = new Coins(cents);
+        String firstCoinString = c1.toString();
+        String secondCoinString = c2.toString();
+        if(firstCoinString.compareTo(secondCoinString) == 0){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
         }
     }
 }
--- RandomStuff/src/test/java/CoinTester.java
+++ RandomStuff/src/test/java/CoinTester.java
@@ -5,8 +5,19 @@ public class CoinTester {
     public static void main(String[] args) {
         Coins c1 = new Coins(25);
         Coins c2 = new Coins(49);
+        Coins c3 = new Coins(78);
+        Coins c4 = new Coins(15);
+        Coins c5 = new Coins(15);
 
+        //Comparing two coins
         c1.compareTo(c2);
-        c1.toString();
+        c3.compareTo(c4);
+        c4.compareTo(c5);
+
+        //Seeing if two coins are equal
+        c4.equals(c5);
+        c1.equals(c3);
+        c2.equals(c3);
+
     }
 }

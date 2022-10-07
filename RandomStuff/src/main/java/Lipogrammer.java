545ea1148fd76a4a8feedeafdd8505007c1f2a20
AriCamhi <arijcamhi@gmail.com>
2022-10-06 20:52:26 -0400

    Finished Lab 4

--- RandomStuff/src/main/java/Lipogrammer.java
+++ RandomStuff/src/main/java/Lipogrammer.java
@@ -1,16 +1,25 @@
 import javax.swing.*;
 
+/**
+ * Lab 4 for ATCS
+ *
+ * @author Ari Camhi
+ * @version 10-6-22
+ */
 public class Lipogrammer {
-    private JTextArea original, marked, words;
-    private JButton go;
+    private JTextArea original;
 
-    // Constructor
+    // Empty constructor
     public Lipogrammer() {
+
     }
+
     public String getText() {
         return original.getText();
     }
+
     public void setText(String text) {
         original.setText(text);
     }
+
 }

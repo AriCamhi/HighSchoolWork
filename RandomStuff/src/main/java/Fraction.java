816951b5996eec8eebfd9fe8d991379f1b9f37a6
AriCamhi <arijcamhi@gmail.com>
2022-10-27 23:56:54 -0400

    Finished Fractions

--- RandomStuff/src/main/java/Fraction.java
+++ RandomStuff/src/main/java/Fraction.java
@@ -8,7 +8,6 @@
  * @version 10-25-22
  */
 public class Fraction {
-
     private int num;
     private int denom;
 
@@ -38,49 +37,27 @@ public class Fraction {
         this.denom = denom;
     }
 
-    public Fraction(double decimal){
-        //Loops for finding the fraction below:
-        double target = 0.0;
-        int goodNum = 0;
-        int goodDenom = 0;
-        for (int num = 1; num <= 100; num++) {
-            for (int denom = 1; denom <= 100; denom++) {
-                double newValue = (double) num / denom;
-                if (newValue < decimal) {
-                    if (newValue > target) {
-                        goodNum = num;
-                        goodDenom = denom;
-                        target = newValue;
-                    }
-                }
-            }
+    /**
+     * Fraction with a decimal as parameter, changes it into
+     * a num and denom
+     *
+     * @param d, a decimal value
+     */
+    public Fraction(double d) {
+        String s = String.valueOf(d);
+        int digitsDec = s.length() - 1 - s.indexOf('.');
+        int denom = 1;
+        for (int i = 0; i < digitsDec; i++) {
+            d *= 10;
+            denom *= 10;
         }
-        //Loops for finding the fraction above:
-        double target2 = 0.3;
-        int betterNum = 0;
-        int betterDenom = 0;
-        for (int i = 1; i <= 100; i++) {
-            for (int j = 1; j <= 100; j++) {
-                double newValueAgain = (double) i / j;
-                if (newValueAgain > decimal) {
-                    if (newValueAgain < target2) {
-                        betterNum = i;
-                        betterDenom = j;
-                        target2 = newValueAgain;
-                    }
-                }
-            }
-        }
-        System.out.println("Fraction below: " + goodNum + "/" + goodDenom);
-        System.out.println("Fraction above: " + betterNum + "/" + betterDenom);
-    }
 
-    public int getNum() {
-        return num;
-    }
-
-    public int getDenom() {
-        return denom;
+        int num = (int) Math.round(d);
+        int j = gcf(num,denom);
+        num = num / j;
+        denom = denom / j;
+        this.num = num;
+        this.denom = denom;
     }
 
     /**
@@ -90,7 +67,7 @@ public class Fraction {
      * @param denom, the denominator
      * @return the greatest common factor
      */
-    private int gcf(int num, int denom)
+    private static int gcf(int num, int denom)
     {
         if (num <= 0 || denom <= 0)
         {
@@ -103,6 +80,185 @@ public class Fraction {
             return num;
         else
             return gcf(num % denom, denom % num);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return getNum() == fraction.getNum() && getDenom() == fraction.getDenom();
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" + num +
+                "/" + denom +
+                '}';
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public int getDenom() {
+        return denom;
+    }
+
+    /**
+     * Gets the decimal value of Fraction
+     * @return decimal value
+     */
+    public double getDecimalValue(){
+        return (double) getNum() / getDenom();
+    }
+
+    /**
+     * Non-static method to add two fraction objects together
+     *
+     * @param other Fraction
+     * @return altered original fraction
+     */
+    public Fraction add(Fraction other) {
+        int f1num = this.getNum();
+        int f1denom = this.getDenom();
+        int f2num = other.getNum();
+        int f2denom = other.getDenom();
+        f1num *= f2denom;
+        f2num *= f1denom;
+        int newDenom = f1denom * f2denom;
+        int newNum = f1num + f2num;
+        int gcf = gcf(newNum,newDenom);
+        newNum = newNum / gcf;
+        newDenom = newDenom / gcf;
+        this.num = newNum;
+        this.denom = newDenom;
+        return this;
+    }
+
+    /**
+     * Non-static method to multiply two fraction objects together
+     *
+     * @param other Fraction
+     * @return altered original fraction
+     */
+    public Fraction multiply(Fraction other){
+        int f1num = this.getNum();
+        int f1denom = this.getDenom();
+        int f2num = other.getNum();
+        int f2denom = other.getDenom();
+        int newNum = f1num * f2num;
+        int newDenom = f1denom * f2denom;
+        int gcf = gcf(newNum,newDenom);
+        newNum = newNum / gcf;
+        newDenom = newDenom / gcf;
+        return new Fraction(newNum, newDenom);
+    }
+
+    /**
+     * Non-static method to divide two fraction objects together
+     *
+     * @param other Fraction
+     * @return altered original fraction
+     */
+    public Fraction divide(Fraction other){
+        int f1num = this.getNum();
+        int f1denom = this.getDenom();
+        int f2num = other.getNum();
+        int f2denom = other.getDenom();
+        int newNum = f1num * f2denom;
+        int newDenom = f1denom * f2num;
+        int gcf = gcf(newNum,newDenom);
+        newNum = newNum / gcf;
+        newDenom = newDenom / gcf;
+        return new Fraction(newNum, newDenom);
+    }
+
+    /**
+     * Print out the fraction in "x/y" form
+     *
+     * @param f1 fraction to be printed
+     */
+    public void printFraction(Fraction f1){
+        System.out.println("Fraction in x/y form: " + f1.toString());
+    }
+
+
+    /**
+     * Method to compare fractions and find which one is greater
+     *
+     * @param other fraction object
+     * @return 1, -1, or 0
+     */
+    public int compareTo(Fraction other) {
+        return Double.compare(this.getDecimalValue(), other.getDecimalValue());
+    }
+
+
+
+    //Static methods below
+
+    /**
+     * Static method to add two fraction objects together
+     *
+     * @param f1 a fraction object
+     * @param f2 a fraction object
+     * @return new fraction
+     */
+    public static Fraction add(Fraction f1, Fraction f2){
+        int f1num = f1.getNum();
+        int f1denom = f1.getDenom();
+        int f2num = f2.getNum();
+        int f2denom = f2.getDenom();
+        f1num *= f2denom;
+        f2num *= f1denom;
+        int newDenom = f1denom * f2denom;
+        int newNum = f1num + f2num;
+        int gcf = gcf(newNum,newDenom);
+        newNum = newNum / gcf;
+        newDenom = newDenom / gcf;
+
+        return new Fraction(newNum, newDenom);
+    }
+
+    /**
+     * Static method to multiply two fraction objects together
+     *
+     * @param f1 a fraction object
+     * @param f2 a fraction object
+     * @return new fraction
+     */
+    public static Fraction multiply(Fraction f1, Fraction f2){
+        int f1num = f1.getNum();
+        int f1denom = f1.getDenom();
+        int f2num = f2.getNum();
+        int f2denom = f2.getDenom();
+        int newNum = f1num * f2num;
+        int newDenom = f1denom * f2denom;
+        int gcf = gcf(newNum,newDenom);
+        newNum = newNum / gcf;
+        newDenom = newDenom / gcf;
+        return new Fraction(newNum, newDenom);
+    }
+
+    /**
+     * Static method to divide two fraction objects together
+     *
+     * @param f1 a fraction object
+     * @param f2 a fraction object
+     * @return new fraction
+     */
+    public static Fraction divide(Fraction f1, Fraction f2){
+        int f1num = f1.getNum();
+        int f1denom = f1.getDenom();
+        int f2num = f2.getNum();
+        int f2denom = f2.getDenom();
+        int newNum = f1num * f2denom;
+        int newDenom = f1denom * f2num;
+        int gcf = gcf(newNum,newDenom);
+        newNum = newNum / gcf;
+        newDenom = newDenom / gcf;
+        return new Fraction(newNum, newDenom);
     }
 }
 
--- RandomStuff/src/test/java/FractionTester.java
+++ RandomStuff/src/test/java/FractionTester.java
@@ -1,3 +1,6 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 /**
  * Tester for the Fraction class to test the methods
  *
@@ -5,9 +8,104 @@
  * @version 10-25-22
  */
 public class FractionTester {
-    Fraction f1 = new Fraction(5);
-    Fraction f2 = new Fraction(0.25);
 
-    //f2.getNum();
+    Fraction f = new Fraction();
+    Fraction f1 = new Fraction(0.25);
+    Fraction f2 = new Fraction(5,8);
+    Fraction f3 = new Fraction(1.4);
+    Fraction f4 = new Fraction(1,4);
 
+    //Non-static tests below
+    @Test
+    public void testingGetters(){
+        assert f1.getNum()==1;
+        assert f1.getDenom()==4;
+    }
+    @Test
+    public void testingDecimalValue(){
+        assert f2.getDecimalValue()==0.625;
+        assert f3.getDecimalValue()==1.4;
+    }
+    @Test
+    public void testingEquality(){
+        testEquality(f1, f1, true);
+        testEquality(f2, f3, false);
+        testEquality(f3, f2, false);
+    }
+
+    private void testEquality(Fraction f1, Fraction f2, boolean expected){
+        Assertions.assertEquals(f1.equals(f2),expected);
+        System.out.println(String.format("%s = %s is %s", f1, f2, expected));
+    }
+    @Test
+    public void testingFractionPrinter(){
+        f1.printFraction(f1);
+        f2.printFraction(f2);
+    }
+
+    @Test
+    public void testingComparisons(){
+        testComparisons(f1, f1, 0);
+        testComparisons(f2, f3, -1);
+        testComparisons(f3, f2, 1);
+    }
+
+    private void testComparisons(Fraction f1, Fraction f2, int expected) {
+        Assertions.assertEquals(f1.compareTo(f2), expected);
+        System.out.println(String.format("Comparison of %s and %s is %s", f1, f2, expected));
+    }
+
+    @Test
+    public void testNonStaticAddition() {
+        System.out.print(String.format("New fraction added from: %s + %s = ", f1, f2));
+        System.out.println("" + f1.add(f2));
+    }
+    @Test
+    public void testNonStaticMultiplication() {
+        System.out.print(String.format("New fraction multiplied from: %s * %s = ", f1, f2));
+        System.out.println("" + f1.multiply(f2));
+    }
+    @Test
+    public void testNonStaticDivision() {
+        System.out.print(String.format("New fraction divided from: %s */* %s = ", f1, f2));
+        System.out.println("" + f1.divide(f2));
+    }
+
+
+    //Static tests below
+    @Test
+    public void testingStaticAddition(){
+        testStaticAdd(f1, f2, new Fraction(7, 8));
+        testStaticAdd(f3, f4, new Fraction(33, 20));
+    }
+
+    private void testStaticAdd(Fraction f1, Fraction f2, Fraction expected) {
+        Fraction result = Fraction.add(f1, f2);
+        Assertions.assertEquals(expected, result);
+        System.out.println(String.format("%s + %s = %s", f1, f2, result));
+    }
+
+    @Test
+    public void testingStaticMultiplication(){
+        testStaticMultiply(f2, f1, new Fraction(5, 32));
+        testStaticMultiply(f3, f2, new Fraction(7, 8));
+    }
+
+    private void testStaticMultiply(Fraction f1, Fraction f2, Fraction expected) {
+        Fraction result = Fraction.multiply(f1, f2);
+        Assertions.assertEquals(expected, result);
+        System.out.println(String.format("%s * %s = %s", f1, f2, result));
+    }
+
+    @Test
+    public void testingStaticDivision(){
+        testStaticDivide(f2, f1, new Fraction(5, 2));
+        testStaticDivide(f3, f2, new Fraction(56, 25));
+    }
+
+    private void testStaticDivide(Fraction f1, Fraction f2, Fraction expected) {
+        Fraction result = Fraction.divide(f1, f2);
+        Assertions.assertEquals(expected, result);
+        System.out.println(String.format("%s */* %s = %s", f1, f2, result));
+    }
 }
